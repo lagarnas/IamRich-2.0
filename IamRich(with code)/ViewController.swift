21f9d24708f38a54e12f287bc06d2292f79546f6
Leonteva Anastasia <lagarnas@gmail.com>
2020-02-16 18:33:56 +0400

    Initial Commit

---
+++ IamRich(with code)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IamRich(with code)
+//
+//  Created by Анастасия Лагарникова on 16.02.2020.
+//  Copyright © 2020 lagarnas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
